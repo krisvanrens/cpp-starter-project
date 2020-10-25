820b98086d1610d1bfaec65f737ee402272ddba6
Kris van Rens <krisvanrens@gmail.com>
2020-10-26 00:31:00 +0100

    CHG: Remove some needless comments. CHG: Catch --> Doctest.

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -1,14 +1,13 @@
-#include <catch2/catch.hpp>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
 
-constexpr unsigned int Factorial(unsigned int number)
-{
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
+#ifdef TEST_NONSTATIC_REQUIRE
+#define STATIC_REQUIRE(assertion) REQUIRE(assertion)
+#else
+#define STATIC_REQUIRE(assertion) static_assert(assertion, #assertion)
+#endif
 
-TEST_CASE("Factorials are computed with constexpr", "[factorial]")
+TEST_CASE("XXX")
 {
-  STATIC_REQUIRE(Factorial(1) == 1);
-  STATIC_REQUIRE(Factorial(2) == 2);
-  STATIC_REQUIRE(Factorial(3) == 6);
-  STATIC_REQUIRE(Factorial(10) == 3628800);
+  STATIC_REQUIRE(true);
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,7 @@
-#include <catch2/catch.hpp>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
 
-unsigned int Factorial(unsigned int number)
+TEST_CASE("XXX")
 {
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
-
-TEST_CASE("Factorials are computed", "[factorial]")
-{
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
+  REQUIRE(true);
 }
